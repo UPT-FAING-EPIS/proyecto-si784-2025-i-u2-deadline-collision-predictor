2077fb0bf408b6e17f6a9ea559ff381165ec40c0
Angel Jose Vargas Gutierrez <av923488264@gmail.com>
2025-06-14 01:28:37 -0500

    version17

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,16 +1,14 @@
-module.exports = function(config) {
-  config.set({
-    mutator: "javascript",
-    packageManager: "npm",
-    reporters: ["html", "clear-text", "progress"],
-    testRunner: "jest",
-    transpilers: [],
-    coverageAnalysis: "off",
-    mutate: ["**/*.js", "!**/node_modules/**", "!**/coverage/**"],
-    thresholds: {
-      high: 80,
-      low: 60,
-      break: 70
-    }
-  });
+module.exports = {
+  mutator: "javascript",
+  packageManager: "npm",
+  reporters: ["html", "clear-text", "progress"],
+  testRunner: "jest",
+  transpilers: [],
+  coverageAnalysis: "off",
+  mutate: ["**/*.js", "!**/node_modules/**", "!**/coverage/**"],
+  thresholds: {
+    high: 80,
+    low: 60,
+    break: 70
+  }
 }; 
