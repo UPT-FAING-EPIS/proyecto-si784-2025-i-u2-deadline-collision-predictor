3ed70d79f5697d79d9fb6eaae77643049b42b3aa
Angel Jose Vargas Gutierrez <av923488264@gmail.com>
2025-06-14 02:13:54 -0500

    version17

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
-  host: '161.132.45.140',
-  user: 'admin',
-  password: 'Upt2025', // Cambia por tu contraseña si tienes
-  database: 'deadline_collision',
+  host: process.env.DB_HOST || '161.132.45.140',
+  user: process.env.DB_USER || 'admin',
+  password: process.env.DB_PASSWORD || 'Upt2025', // Cambia por tu contraseña si tienes
+  database: process.env.DB_NAME || 'deadline_collision',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
